8e1b2e2a4d80a62437730c7f3088572deb6b690b
Julian Brown <julian.brown@cpanel.net>
2021-07-07 15:50:21 -0500

    removed some debug stuff

--- debify/debian/override_dh_auto_config.sh
+++ debify/debian/override_dh_auto_config.sh
@@ -15,18 +15,9 @@ cp /usr/share/pkgconfig/ea-apr16-util-1.pc config/apr-util-1.pc
 cp /usr/share/pkgconfig/ea-apr16-1.pc config
 cp /usr/share/pkgconfig/ea-apr16-util-1.pc config
 
-echo "APRPKGCONFIG: V2"
-cat -n /usr/share/pkgconfig/ea-apr16-1.pc
-cat -n /usr/share/pkgconfig/ea-apr16-util-1.pc
-find /opt/cpanel -name "libtool" -print 2>/dev/null
-
 export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:`pwd`/config"
 touch configure
 touch configure.apxs
-
-echo "look for libtool: 1"
-find . -type f -print | xargs grep libtool
-echo "look for libtool: 2"
 
 export APR_CONFIG=/opt/cpanel/ea-apr16/bin/apr-1-config
 export libtool=/opt/cpanel/ea-apr16/lib64/apr-1/build/libtool
